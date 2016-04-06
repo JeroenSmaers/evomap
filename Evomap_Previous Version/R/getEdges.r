4583a4ac9a86a8b258a4919b038310bdd3c42f51
cmongle <carrie.mongle@stonybrook.edu>
2016-04-06 18:24:42 -0400

    Updating package

--- Evomap_Previous Version/R/getEdges.r
+++
@@ -1,7 +0,0 @@
-getEdges<-function(tree,node){
-      Desc<-getDescendants(tree,node)
-      Edges<-c()
-      for(i in 1:length(Desc)){Edges<-c(Edges,which(tree$edge[,2]==Desc[i]))}
-      Edges<-c(Edges,which(tree$edge[,2]==node))
-      return(Edges)}
-      
--- R/getEdges.r
+++ R/getEdges.r
@@ -1,3 +1,11 @@
+#' List all edges ancestral to a node
+#'
+#' @param tree an object of class 'phylo'.
+#' @param node number of interest.
+#' @return vector of edge numbers.
+
+#' @export
+
 getEdges<-function(tree,node){
       Desc<-getDescendants(tree,node)
       Edges<-c()
