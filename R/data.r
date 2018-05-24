42639d7b6272dd6bdac0575f9f8ef2bf622d2fcf
cmongle <carrie.mongle@stonybrook.edu>
2018-05-24 12:40:38 -0400

    data sources

---
+++ R/data.r
@@ -0,0 +1,28 @@
+#' Isler et al. (2008) dataset of primate brain and body mass.
+#'
+#' A dataset containing the brain and body mass of 74 primate species
+#' 
+#'
+#' @format A data frame with 74 rows and 2 variables:
+#' \describe{
+#'   \item{Brain}{brain mass, in grams}
+#'   \item{Body}{body mass, in grams}
+#'   ...
+#' }
+#' @source Isler K, Kirk EC, Miller JMA, Albrecht GA, Gelvin BR, Martin RD. 2008. Endocranial volumes of primate species: scaling analyses using a comprehensive and reliable data set. Journal of Human Evolution 55: 967-978.
+"primateData"
+
+
+
+#' Molecular consensus phylogeny of primate species from the 10k trees project
+#'
+#' An object of class "phylo"
+#' 
+#'
+#' @format A tree  146 primate species
+#'   ...
+#' }
+#' @source Arnold C, Matthews LJ, Nunn CL. 2010. The 10k Trees Website: A New Online Resource for Primate Phylogeny. Evolutionary Anthropology 19: 114-118.
+"primateTree"
+
+
