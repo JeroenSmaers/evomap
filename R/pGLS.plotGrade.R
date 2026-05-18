adeadc5ce8c45afb0769c03c297004f418925953
Jeroen <jeroen@mac.mynetworksettings.com>
2026-05-18 17:47:24 -0400

    Replace gls/nlme with phylolm in pGLS.plotGrade
    Switches from gls() with corBrownian/corPagel to phylolm() for both BM
    and lambda models. Updates coefficient extraction from tTable to
    coefficients matrix. Adds phylolm to DESCRIPTION imports, removes nlme.

--- R/pGLS.plotGrade.R
+++ R/pGLS.plotGrade.R
@@ -11,17 +11,22 @@
 #' @examples see smaerslab.com/software
 
 #' @export
-pGLS.plotGrade<-function(Yvar,Xvar,data,tree,model,group,...){
-    dataTemp<-pruneSample(na.omit(data),tree,group)$data
-    treeTemp<-pruneSample(dataTemp,tree,group)$tree
-    Y<-dataTemp[,which(colnames(dataTemp)==paste(Yvar))]
-    X<-dataTemp[,which(colnames(dataTemp)==paste(Xvar))]
-    dataGLS<-as.data.frame(cbind(Y,X)); rownames(dataGLS)<-rownames(dataTemp)
-switch(model,
-  BM={pGLSTemp<-gls(Y~X,dataGLS,correlation=corBrownian(phy=treeTemp))},
-  lambda={pGLSTemp<-gls(Y~X,dataGLS,correlation=corPagel(1,phy=treeTemp,fixed=FALSE))})
-      a<-summary(pGLSTemp)$tTable[1,1]
-      b<-summary(pGLSTemp)$tTable[2,1]
-      lines(c(min(X),max(X)),c((a+b*min(X)),(a+b*max(X))),...)
-      points(X,Y,...)
-  }
+pGLS.plotGrade<-function (Yvar, Xvar, data, tree, model, group, ...)
+{
+    dataTemp <- pruneSample(na.omit(data), tree, group)$data
+    treeTemp <- pruneSample(dataTemp, tree, group)$tree
+    Y <- dataTemp[, which(colnames(dataTemp) == paste(Yvar))]
+    X <- dataTemp[, which(colnames(dataTemp) == paste(Xvar))]
+    dataGLS <- as.data.frame(cbind(Y, X))
+    rownames(dataGLS) <- rownames(dataTemp)
+    switch(model, BM = {
+        pGLSTemp <- phylolm(Y ~ X, data=dataGLS, phy=treeTemp, model = "BM")
+        }, lambda = {
+        pGLSTemp <- phylolm(Y ~ X, data=dataGLS, phy=treeTemp, model = "lambda")
+        })
+    a <- summary(pGLSTemp)$coefficients[1, 1]
+    b <- summary(pGLSTemp)$coefficients[2, 1]
+    lines(c(min(X), max(X)), c((a + b * min(X)), (a + b * max(X))),
+        ...)
+    points(X, Y, ...)
+}
